f2222ecbc73fb4eb0edd75afd133a6b26de5978f
Adrien Sales <salad74@ville-noumea.nc>
2014-07-10 00:25:30 +0000

    Implémentation de tests uniatires, cf #5447... et correction du pom pour que le driver jt400 soit résolu via systemPath, en 8.2 et ainsi que le buid passe sans avoir à manuellement installer la dépendance, cf #8495

---
+++ src/test/java/nc/noumea/mairie/distiller/DistillerTest.java
@@ -0,0 +1,213 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nc.noumea.mairie.distiller;
+
+import com.lowagie.text.pdf.PdfReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Date;
+import jcifs.smb.NtlmPasswordAuthentication;
+import jcifs.smb.SmbFile;
+import jcifs.smb.SmbFileInputStream;
+import jcifs.smb.SmbFileOutputStream;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+/**
+ *
+ * @author salad74
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class DistillerTest {
+
+    private Configuration conf;
+    private String testFileName;
+    private String destinationPath;
+    private String as400User;
+    private NtlmPasswordAuthentication auth;
+    private String pdfDirectory;
+
+    /**
+     * Uploads test image to remote samba share
+     */
+    @Before
+    public void init() {
+        try {
+            conf = new PropertiesConfiguration("distiller.properties");
+            testFileName = conf.getString("image.to.distill");
+            Assert.assertNotNull(testFileName);
+
+            destinationPath = conf.getString("target.directory");
+            Assert.assertNotNull(destinationPath);
+
+            as400User = conf.getString("as400.user");
+            Assert.assertNotNull(as400User);
+
+            auth = new NtlmPasswordAuthentication(as400User);
+            Assert.assertNotNull(auth);
+            
+            pdfDirectory = conf.getString("pdf.directory");
+            Assert.assertNotNull(pdfDirectory);
+            
+
+        } catch (ConfigurationException ex) {
+            conf = null;
+        }
+    }
+
+    @Test
+    public void aTestTestConf() {
+        Assert.assertNotNull(conf);
+    }
+
+    @Test
+    public void bCleanUpTestFiles(){
+        // delete input test image
+        try{
+            SmbFile dFile = new SmbFile(destinationPath + "/" + testFileName, auth);
+            if(dFile.exists()){
+                dFile.delete();
+            }
+
+            dFile = new SmbFile(destinationPath + "/" + testFileName, auth);
+            Assert.assertFalse(dFile.exists());
+        }
+        catch(Exception ex){
+            Assert.assertNull(ex);
+        }
+        
+        // delete distilled pdf
+        try{
+            String pdfFileName = FilenameUtils.removeExtension(testFileName) + ".pdf";
+            SmbFile sFile = new SmbFile(pdfDirectory + pdfFileName, auth);
+            if(sFile.exists()){
+                sFile.delete();
+            }
+            sFile = new SmbFile(pdfDirectory + pdfFileName, auth);
+            Assert.assertFalse(sFile.exists());
+        }
+        catch(Exception ex){
+            Assert.assertNull(ex);
+        }
+    }
+    @Test
+    public void cUploadImage() {
+        try {
+            System.out.println("testFileName : " + testFileName);
+            System.out.println("destinationPath : " + destinationPath);
+            System.out.println("as400User : " + as400User);
+            
+            URL url = getClass().getResource("/" + testFileName);
+            File testFile = new File(url.getFile());
+            Assert.assertTrue(testFile.exists());
+                    
+            FileInputStream stream = new FileInputStream(testFile);
+
+            SmbFile dFile = new SmbFile(destinationPath + "/" + testFileName, auth);
+            SmbFileOutputStream outStream = new SmbFileOutputStream(dFile);
+            outStream.write(IOUtils.toByteArray(stream));
+            outStream.flush();
+            outStream.close();
+            stream.close();
+            Assert.assertTrue(dFile.exists());
+            Assert.assertTrue(dFile.canRead());
+            Assert.assertNotNull(dFile.getUncPath());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Assert.assertNull(ex);
+        }
+        
+        // image should have been uploaded
+        
+    }
+
+
+    @Test
+    public void dWaitForDistiller(){
+        try{
+            Date start = new Date();
+            
+            boolean exists = true;
+            do {
+                SmbFile sFile = new SmbFile(destinationPath + "/" + testFileName, auth);
+                exists = sFile.exists();
+                System.out.println("image still waiting for being processed by distiller.");
+                Thread.sleep(500);
+            } while (exists);
+            Date end = new Date();
+            SmbFile sFile = new SmbFile(destinationPath + "/" + testFileName, auth);
+            Assert.assertFalse(sFile.exists());
+            System.out.println("image now being processed by distiller.");
+        }
+        catch(Exception ex){
+            Assert.assertNull(ex);
+            ex.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void eCheckDistillation(){
+        try{
+            Date start = new Date();
+            
+            boolean distilled = false;
+            String pdfFileName = FilenameUtils.removeExtension(testFileName) + ".pdf";
+            
+            System.out.println("pdf filename : " + pdfDirectory + pdfFileName);
+            SmbFile sFile = new SmbFile(pdfDirectory + pdfFileName, auth);
+            distilled = sFile.exists();
+            while(distilled == false) {
+                sFile = new SmbFile(pdfDirectory + pdfFileName, auth);
+                distilled = sFile.exists();
+                System.out.println("image still being processed by distiller...");
+                Thread.sleep(100);
+            }
+            Date end = new Date();
+            sFile = new SmbFile(pdfDirectory + pdfFileName, auth);
+            Assert.assertTrue(sFile.exists());
+            System.out.println("image has been been processed by distiller into a pdf.");
+        }
+        catch(Exception ex){
+            Assert.assertNull(ex);
+            ex.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void fTestPdfNotCorrupted(){
+        // get the stream of the smb file
+        try{
+            String pdfFileName = FilenameUtils.removeExtension(testFileName) + ".pdf";
+            SmbFile sFile = new SmbFile(pdfDirectory + pdfFileName, auth);
+            SmbFileInputStream stream = new SmbFileInputStream(sFile);
+            
+            PdfReader reader = new PdfReader(stream);
+            Assert.assertNotNull(reader);
+            int nbPages = reader.getNumberOfPages();
+            Assert.assertTrue("A not corrupted pdf has at least one page", nbPages > 0);
+            reader.close();
+            stream.close();
+        
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            Assert.assertNull(ex);
+        }
+        
+    }
+
+}
